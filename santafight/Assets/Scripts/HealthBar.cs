3f109c570c4597ecd74ac8c3c6c48fe6994ff8d1
Tiago Granja <tiagogranja95@gmail.com>
2022-06-02 13:00:07 +0100

    First Commit
    - 3D character
    - movement e animações
    - animação de ataque
    - Adicionado a barra de vida
    - Adicionado uma função para levar dano
    - Animações quando o player leva dano
    - Animação da morte

---
+++ santafight/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace StarterAssets
+{
+    public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetMaxHealth (int maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+
+    public void SetCurrentHealth(int currentHealth)
+    {
+        slider.value = currentHealth;
+    }
+}
+}
---
+++ santafight/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int healthLevel = 10;
+        public int maxHealth;
+        public int currentHealth;
+
+        public HealthBar healthbar;
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        void Start()
+        {
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth;
+            healthbar.SetMaxHealth(maxHealth);
+        }
+
+        private int SetMaxHealthFromHealthLevel()
+        {
+            maxHealth = healthLevel * 10;
+            return maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth = currentHealth - damage;
+
+            healthbar.SetCurrentHealth(currentHealth);
+
+            _animator.SetTrigger("Damage");
+
+            if(currentHealth <= 0)
+            {
+                currentHealth = 0;
+                _animator.SetTrigger("Die");
+                //handle player death
+            }
+            
+        }
+    }
+}
+
